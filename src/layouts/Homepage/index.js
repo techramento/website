ae5540b15e95c627f60581d0790242834558cc35
Derek Lindahl <derek.lindahl@gmail.com>
2017-05-04 22:03:06 -0700

    Add and initialize Phenomic site

---
+++ src/layouts/Homepage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import LatestPosts from '../../components/LatestPosts'
+import Page from '../Page'
+
+const Homepage = (props) => (
+  <Page {...props}>
+    <LatestPosts/>
+  </Page>
+  )
+
+export default Homepage
