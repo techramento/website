81bbde2e9f30e5ce9b5f20b2d299161ecc6141fe
Derek Lindahl <derek.lindahl@gmail.com>
2017-05-17 14:33:54 -0700

    Add custom hero images to Events, Join Us, and Mission pages

--- src/metadata.js
+++ src/metadata.js
@@ -26,21 +26,21 @@ export default {
     },
     {
       description: 'Discover the many events that we host and facilitate in the Sacramento area.',
-      hero: 'https://farm4.staticflickr.com/3949/15589950511_3675b15e59_k.jpg',
+      hero: '/assets/images/events/hero@2x.jpg',
       label: 'Events',
       path: '/events',
       title: 'Events'
     },
     {
       description: 'Learn how to join our ranks as stewards of the Sacramento Tech Community.',
-      hero: '/assets/images/community/hero@2x.jpg',
+      hero: '/assets/images/join/hero@2x.jpg',
       label: 'Join',
       path: '/join',
       title: 'Join Us'
     },
     {
       description: `See how our community leaders are mentoring and training Sacramento's tech scene.`,
-      hero: 'https://farm4.staticflickr.com/3949/15589950511_3675b15e59_k.jpg',
+      hero: '/assets/images/mission/hero@2x.jpg',
       label: 'Mission',
       path: '/mission',
       title: 'Our Mission'
